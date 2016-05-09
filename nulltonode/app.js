4fe806c6b3d76e687ccb4866da26025c1c0ddfee
jaketarnow <jtarnow@dons.usfca.edu>
2016-05-09 12:36:30 -0700

    refactoring

--- nulltonode/app.js
+++ nulltonode/app.js
@@ -4,7 +4,8 @@ var app = express();
 var routes = require('./routes');
 var hbs = require('express3-handlebars');
 
-//Set handlebars as the default template engine --> use main.handlebars as default
+// Set handlebars as the default templating engine
+// and use main.handlebars as our default layout.
 app.engine('handlebars', hbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
--- nulltonode/routes.js
+++ nulltonode/routes.js
@@ -1,4 +1,4 @@
 /* file for routes of app.js */
 exports.home = function (request, response) {
-	response.render('home');
+  response.render('home');
 }
