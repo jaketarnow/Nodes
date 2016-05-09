33240c44e192f7f899f9048d616033896d439ea6
jaketarnow <jtarnow@dons.usfca.edu>
2016-05-09 12:17:48 -0700

    edits

--- nulltonode/app.js
+++ nulltonode/app.js
@@ -1,10 +1,14 @@
 /* require is a NodeJS specific funtion used to load pckg */
 var express = require('express');
 var app = express();
+var routes = require('./routes');
+var hbs = require('express-handlebars');
 
-app.get('/hello', function (request, response) {
-	response.send('Hello World');
-});
+//Set handlebars as the default template engine --> use main.handlebars as default
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', routes.home);
 
 app.listen(3000);
 console.log('App running on port 3000');
---
+++ nulltonode/routes.js
@@ -0,0 +1,4 @@
+/* file for routes of app.js */
+exports.home = function (request, response) {
+	response.render('home');
+}
